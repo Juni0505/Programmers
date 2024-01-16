db4f8ada54f486a7985affa00c5c0048f727613f
Juni0505 <dudwns050505@gmail.com>
2024-01-16 23:29:54 +0900

    Programmers level0 and level1
    Solve

---
+++ src/level0/DistinguishwithSpace.java
@@ -0,0 +1,7 @@
+package level0;
+
+public class DistinguishwithSpace {
+	 public String[] solution(String my_string) {
+	        return my_string.split(" ");
+	    }
+}
--- src/level1/SortStr.java
+++ src/level1/SortStr.java
@@ -8,13 +8,10 @@ public class SortStr {
 	public String[] solution(String[] strings, int n) {
         String[] answer = new String[strings.length];
         List<String> list = new ArrayList<>();
-        
         for(int i = 0; i < strings.length; i++) {
             list.add(strings[i].charAt(n) + strings[i]);
         }
-        
         Collections.sort(list);
-        
         for(int i = 0; i < list.size(); i++) {
             answer[i] = list.get(i).substring(1);
         }
