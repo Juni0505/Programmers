2d57c9dc8eecdec0dc88dddccbba49df09a0819f
Juni0505 <dudwns050505@gmail.com>
2024-02-12 18:55:15 +0900

    Programmers level0
    Solve

---
+++ src/level0/ThreeSixNineGame.java
@@ -0,0 +1,12 @@
+package level0;
+
+public class ThreeSixNineGame {
+	public int solution(int order) {
+        int count = 0;
+        while (order != 0) {
+            if (order % 10 == 3 || order % 10 == 6 || order % 10 == 9) count++;
+            order /= 10;
+        }
+        return count;
+    }
+}
