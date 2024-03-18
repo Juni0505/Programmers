3124c4363cd6457b94f85c74889ce555023be64a
Juni0505 <dudwns050505@gmail.com>
2024-03-18 23:55:03 +0900

    Programmers
    Solve

---
+++ src/level0/HateEnglish.java
@@ -0,0 +1,29 @@
+package level0;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HateEnglish {
+	public int solution(String numbers) {
+        String[] numberWords = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        Map<String, Integer> numberMap = new HashMap<>();
+        
+        for (int i = 0; i < 10; i++) {
+            numberMap.put(numberWords[i], i);
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        while (index < numbers.length()) {
+            for (int i = 1; i <= 10; i++) {
+                if (numberMap.containsKey(numbers.substring(index, index + i))) {
+                    sb.append(numberMap.get(numbers.substring(index, index + i)));
+                    index += i;
+                    break;
+                }
+            }
+        }
+        
+        return Integer.parseInt(sb.toString());
+    }
+}
