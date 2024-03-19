b99b21aa6af89caf9413f39ffa1bf553a8db7fd7
Juni0505 <dudwns050505@gmail.com>
2024-03-19 23:49:05 +0900

    Programmer
    Solve

---
+++ src/level0/Emergency.java
@@ -0,0 +1,21 @@
+package level0;
+
+import java.util.Arrays;
+
+public class Emergency {
+	public int[] solution(int[] emergency) {
+        int[] result = new int[emergency.length];
+        
+        for (int i = 0; i < emergency.length; i++) {
+            int rank = 1; 
+            for (int j = 0; j < emergency.length; j++) {
+                if (emergency[j] > emergency[i]) {
+                    rank++; 
+                }
+            }
+            result[i] = rank; 
+        }
+        
+        return result;
+    }
+}
