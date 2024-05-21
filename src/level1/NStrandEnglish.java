77bf3ba975b8b8dbffbf60828e077df7ef17eac3
Juni0505 <dudwns050505@gmail.com>
2024-05-21 23:55:37 +0900

    Programmers
    Level1 Solve

---
+++ src/level1/NStrandEnglish.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class NStrandEnglish {
+	 public int solution(String s) {
+	        String[] words = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+	        
+	        for (int i = 0; i < words.length; i++) {
+	            s = s.replace(words[i], String.valueOf(i));
+	        }
+	        
+	        return Integer.parseInt(s);
+	    }
+}
